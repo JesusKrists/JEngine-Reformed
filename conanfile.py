4751b8974d44d5f5cda84932c2a9f47e26fc4c99
JesusKrists <krists.n99@gmail.com>
2022-12-25 16:42:03 +0100

    Conan update

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,25 @@
 from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.build import check_min_cppstd
 
 
 class Recipe(ConanFile):
+    name = "JEngine-Reformed"
+    version = "0.0.1"
+    description = "The next generation of JEngine"
+    homepage = "https://jesuskrists.github.io/JEngine-Reformed/"
+    url = "https://github.com/JesusKrists/JEngine-Reformed"
+
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
 
     default_options = {"tracy:shared": True, "tracy:callstack": True}
 
+    def source(self):
+        self.run("git clone https://github.com/JesusKrists/JEngine-Reformed.git")
+
     def layout(self):
+        cmake_layout(self, src_folder="source")
         self.folders.generators = "conan"
 
     def requirements(self):
@@ -17,3 +29,20 @@ class Recipe(ConanFile):
 
     def build_requirements(self):
         self.test_requires("catch2/3.1.0")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def validate(self):
+        if self.info.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, 20)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
