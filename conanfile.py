bcd90df41a1a9b079355a07e63a047dbab0a93ae
kniedrit <kniedrit@volvocars.com>
2023-02-08 11:14:44 +0100

    Le conan flex

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,17 @@ class Recipe(ConanFile):
 
     keep_imports = True
 
+    options = {
+        "dev_build": [True, False],
+        "coverage_build": [True, False],
+        "sanitize_build": [True, False],
+    }
+
     default_options = {
+        "dev_build": False,
+        "coverage_build": False,
+        "sanitize_build": False,
+        ##########################
         "spdlog:shared": True,
         "fmt:shared": True,
         "tracy:shared": True,
@@ -29,25 +39,38 @@ class Recipe(ConanFile):
     def requirements(self):
         self.requires("spdlog/1.11.0")
         self.requires("sdl/2.26.1")
-        self.requires("tracy/0.9")
         self.requires("nanosvg/cci.20210904")
+        self.requires("tracy/0.9")
 
     def build_requirements(self):
         self.test_requires("catch2/3.1.0")
 
     def imports(self):
-        self.copy("*.dll", "build/Debug", "bin")
-        self.copy("*.dll", "build/test/Debug", "bin")
-        self.copy("*.dll", "build/Release", "bin")
-        self.copy("*.dll", "build/test/Release", "bin")
-
-        self.copy("*.dll", "build/dev/", "bin")
-        self.copy("*.dll", "build/dev/test", "bin")
-        self.copy("*.so*", "build/dev/", "lib")
-        self.copy("*.so*", "build/dev/test", "lib")
-
-        self.copy("*.so*", "build/", "lib")
-        self.copy("*.so*", "build/test", "lib")
+        build_folder = "build/"
+        test_folder = "build/test"
+        if self.options.dev_build:
+            build_folder = "build/dev"
+            test_folder = "build/dev/test"
+        if self.options.coverage_build:
+            build_folder = "build/coverage"
+            test_folder = "build/coverage/test"
+        if self.options.sanitize_build:
+            build_folder = "build/sanitize"
+            test_folder = "build/sanitize/test"
+        if (
+            is_msvc(self)
+            and not self.options.dev_build
+            and not self.options.coverage_build
+            and not self.options.sanitize_build
+        ):
+            build_folder = f"build/{str(self.settings.build_type)}"
+            test_folder = f"build/test/{str(self.settings.build_type)}"
+        if self.settings.os == "Windows":
+            self.copy("*.dll", build_folder, "bin")
+            self.copy("*.dll", test_folder, "bin")
+        if self.settings.os == "Linux":
+            self.copy("*.so*", build_folder, "lib")
+            self.copy("*.so*", test_folder, "lib")
 
     def config_options(self):
         if not is_msvc(self):
